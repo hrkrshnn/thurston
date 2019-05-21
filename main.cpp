8f98e1b70aa03604606f108485c5fca55eab842c
Harikrishnan Mulackal <harikrishnan@localhost.localdomain>
2019-05-21 21:50:37 +0200

    Added transformation function

--- main.cpp
+++ main.cpp
@@ -7,15 +7,88 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
 
+// For complex numbers
+#include <boost/multiprecision/mpc.hpp>
+#include <boost/multiprecision/mpfr.hpp>
 
-
-// [a b]
-// [c d]
-// ad - bc = M
 template <typename Z>
-auto genPoints(Z M, Z range = 20)
+using matrix = boost::numeric::ublas::matrix<Z>;
+namespace mp = boost::multiprecision;
+
+// 6th root of unity
+const mp::mpc_complex omega{0.5, std::sqrt(3)/2};
+const std::size_t iterMax = 1000;
+
+template <typename Z>
+auto matToComplex(const matrix<Z>& mat)
 {
-  std::vector<boost::numeric::ublas::matrix<Z>> results;
+  mp::mpc_complex num = (mat(1, 0) + omega*mat(1, 1))/(mat(0, 0) + omega*mat(0, 1));
+  std::cout<<"matrix: "<<mat<<" complex: "<<num<<std::endl;
+}
+
+template <typename Z>
+auto fundTransform(matrix<Z>& mat)
+{
+  // Do an inversion operation on the matrix; Inversion in the hyperbolic sense
+  auto inversion = [&mat]()
+                {
+                  matrix<Z> tmp = mat;
+                  mat(0, 0) = -tmp(1, 0);
+                  mat(0, 1) = -tmp(1, 1);
+                  mat(1, 0) = tmp(0, 0);
+                  mat(1, 1) = tmp(1, 0);
+                };
+
+  // Do a z \-> z + k
+  // Do translation by k
+  auto translate = [&mat](Z k)
+                   {
+                     mat(1, 0) += mat(0, 0) * k;
+                     mat(1, 1) += mat(0, 1) * k;
+                   };
+
+  // Converts matrix into a complex number (c + d*omega)/(a + b*omega)
+  auto toComplex = [&mat]()
+                   {
+                     mp::mpc_complex num = (mat(1, 0) + omega*mat(1, 1))/(mat(0, 0) + omega*mat(0, 1));
+                     return num; // std::move(num)?
+                   };
+
+  std::size_t iter = 0;
+  while(iter < iterMax)
+    {
+      auto z = toComplex();
+      if(mp::abs(z) < 1)
+        {
+          inversion();
+        }
+      else if(z.real() < -0.5)
+        {
+          Z k = mp::floor(z.real() + 0.5); // TODO: check the calculation
+          translate(-k);
+          // do a translation
+        }
+      else if(z.real() > 0.5)
+        {
+          Z k = mp::floor(z.real() - 0.5);
+          translate(-k);
+        }
+      else                      // the number is in the fundamental region
+        {
+          break;
+        }
+
+      ++iter;
+    }
+
+
+      // I don't need to return anything, whatever matrix was called, got transformed
+}
+
+template <typename Z>
+void genPoints(Z M, Z range)
+{
+  std::vector<matrix<Z>> results;
 
   for(Z a1 = 1; a1 < M + range; ++a1)
     {
@@ -31,7 +104,7 @@ auto genPoints(Z M, Z range = 20)
           auto a = a1*M/gcd;      // TODO Cautious about integer overflow?
           auto b = b1*M/gcd;
 
-          boost::numeric::ublas::matrix<Z> mat(2, 2);
+          matrix<Z> mat(2, 2);
           mat(0, 0) = a;
           mat(0, 1) = -b;
           mat(1, 0) = c;
@@ -51,10 +124,41 @@ auto genPoints(Z M, Z range = 20)
     {
       std::cout<<v<<"\n";
     }
+
+  for(auto& v: results)
+    {
+      fundTransform(v);
+    }
+
+  std::cout<<"Transformed";
+  for(auto& v: results)
+    {
+      std::cout<<v<<"\n";
+    }
+
 }
+
+
 
 auto main() -> int
 {
-  genPoints(4, 20);
+  // matrix<int> test(2, 2);
+  // test(0, 0) = 2;
+  // test(0, 1) = 0;
+  // test(1, 0) = 0;
+  // test(1, 1) = 1;
+
+  // std::cout<<test<<"\n";
+  // matToComplex(test);
+
+  // fundTransform(test);
+  // std::cout<<"After transform\n";
+  // std::cout<<test;
+  // matToComplex(test);
+  genPoints(4, 0);
   return 0;
 }
+
+// [a b]
+// [c d]
+// ad - bc = M
